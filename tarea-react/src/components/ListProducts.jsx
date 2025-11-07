b31fdcbabcdc61a519b982896d309ab0d21b1c57
Facundo Vera <facundovera.dev@gmail.com>
2025-11-07 17:44:22 -0300

    Ejercicio useEffect,localStorage

--- tarea-react/src/components/ListProducts.jsx
+++ tarea-react/src/components/ListProducts.jsx
@@ -3,33 +3,35 @@ import CardProducts from "./CardProducts"
 import {  useEffect, useState } from "react";
 
 const ListProducts = () => {
-  const [producto, setProducto] = useState([])
+  const [producto, setProducto] = useState([]);
 
   useEffect(() => {
-    if(!localStorage.getItem("producto")){
-      localStorage.setItem("producto",JSON.stringify(products))
-    }else{
+    if (!localStorage.getItem("producto")) {
+      localStorage.setItem("producto", JSON.stringify(products));
+      setProducto(products); 
+    } else {
       setProducto(JSON.parse(localStorage.getItem("producto")));
     }
-  }, [])
+  }, []);
 
-  const updateSave = (id)=> {
+  const updateSave = (id) => {
+    const update = producto.map((product) =>
+      product.id === id ? { ...product, save: !product.save } : product
+    );
 
-    const update = producto.map((product) => {
-      //&me devuelve todo lo que viene en user y que a like la ramplace por lo opuesto a user.like 
-      return product.id === id ? { ...product, save: !product.save } : product;
-    });
-     //*guardo en estado usuario el update
     setProducto(update);
-    //*Actualizo el estado en el local
     localStorage.setItem("producto", JSON.stringify(update));
+  };
 
-  }
   
   return (
     <div className="list">
-      {products.map((product) => (
-       <CardProducts product={product} key={product.id} updateSave={updateSave} />
+      {producto.map((product) => ( 
+        <CardProducts
+          product={product}
+          key={product.id}
+          updateSave={updateSave}
+        />
       ))}
     </div>
   );
